699265a47ab555c5798b2da3ef330102f4d5bd37
Gabrielle Sutherland <gsutherland@pitt.edu>
2026-03-03 13:47:36 -0500

    updated with basic code

---
+++ main.py
@@ -0,0 +1,8 @@
+nameQ = input("What's your name?")
+ageQ = int(input("How old are you?"))
+
+print (f"Your name is" + nameQ)
+print (f"Your age is" + ageQ)
+if ageQ = int:
+    print(f"In 20 years you will be " + (20 + ageQ))
+
